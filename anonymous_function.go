2ee8228502b9277ca2220dbc400c566baaa6acc5
Rozzi21 <fakhruddinarrozi21@gmail.com>
2024-04-27 01:02:45 +0700

    new

---
+++ anonymous_function.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Blacklist func(string2 string) bool
+
+func registerUser(name string, blacklist Blacklist) {
+	if blacklist(name) {
+		fmt.Println("You are blocked", name)
+	} else {
+		fmt.Println("halo", name)
+	}
+}
+
+func main() {
+	kata := func(name string) bool {
+		{
+			return name == "asu"
+		}
+	}
+	registerUser("jhon", kata)
+}
